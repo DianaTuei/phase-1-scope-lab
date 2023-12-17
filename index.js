f006034a5d7aa66b7b919db96dadc4958aebb194
DianaTuei <memotuei@gmail.com>
2023-12-17 10:11:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "bernard";
+
+function upperCaseCustomerName(){
+    if(customerName){
+     customerName = customerName.toUpperCase();
+     console.log("Uppercased customerName:", customerName);
+    }else{
+        console.log(undefined);
+    }
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "leonida";
+}
